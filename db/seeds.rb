8f38e6829dcd288a892f3aa092e69bd9f4bda75d
Brian Mont <briansmont830@gmail.com>
2016-09-16 20:40:04 +0000

    Rails Assignment 17 - Unique body & title exercise using find_or_create_by method, rake db:seed to confirm only one occurrence of each unique item

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,9 @@ posts = Post.all
     )
 end
 
+Post.find_or_create_by(title: "unique title", body: "unique body")
+Comment.find_or_create_by(body: "unique comment body")
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
