fba63920edd9d228648a5eca75358cb7c18e43a8
Brian Mont <briansmont830@gmail.com>
2016-09-20 16:41:20 +0000

    Rails Assignment 18 - PostsController selects index-0 & every 5th following, changing post.title to 'SPAM', changes in view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,12 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    #each 5th is spam
+    @posts.each_with_index do |post, index|
+      if index == 0 || index % 5 == 0
+        post.title = "SPAM"
+      end
+    end
   end
 
   def show
