357d15ceac180878f2263b6d4dcc9b420b272eab
Brian Mont <briansmont830@gmail.com>
2016-09-26 19:17:22 +0000

    Rails Assignment 22 - SponsoredPosts model&controller created, foreign key linking each sponsoredPost to its topic, CRUD functionality dev'd using TDD

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,15 @@ require 'random_data'
 end
 topics = Topic.all
 
+#create sponsored posts
+50.times do
+  SponsoredPost.create!(
+    topic: topics.sample,
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph
+    )
+end
+
 #create posts
 50.times do
   Post.create!(
@@ -31,4 +40,5 @@ end
 puts "Seed finished"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
+puts "#{SponsoredPost.count} sponsored posts created"
 puts "#{Comment.count} comments created"
